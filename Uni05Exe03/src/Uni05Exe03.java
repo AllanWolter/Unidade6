145b28d168e14b5a975d25ae69d2ae80f737c7e4
AllanWolter <desenvolvimentoallanbs@gmail.com>
2026-05-11 20:43:47 -0300

    exe03

--- Uni05Exe03/src/Uni05Exe03.java
+++ Uni05Exe03/src/Uni05Exe03.java
@@ -1,42 +1,46 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uni05Exe03 {
     public Uni05Exe03() {
         Scanner sc = new Scanner(System.in);
-        double[] vetor = new double[12];
+        double[] vetor = new double[3];
 
         lervalores(sc, vetor);
 
         calcularvalores(vetor);
 
-        escrevervalores();
+        escrevervalores(vetor);
 
         sc.close();
 
     }
+
     private void lervalores(Scanner sc, double[] vetor) {
         for (int i = 0; i < vetor.length; i++) {
-            System.out.print("Digite o " + (i + 1) +"º valor: ");
+            System.out.print("Digite o " + (i + 1) + "º valor: ");
             vetor[i] = sc.nextDouble();
         }
     }
+
     private void calcularvalores(double[] vetor) {
         for (int i = 0; i < vetor.length; i++) {
-            if ((vetor[i] % 2) == 0){
+            if (((i + 1) % 2) == 0) {
                 vetor[i] *= 1.02;
             } else {
                 vetor[i] *= 1.05;
             }
-        } 
+        }
     }
-    private void escrevervalores() {
-        for (int i = 0; i < vetor.length; i++) {
 
-            System.out.println(vetor[i]);
+    private void escrevervalores(double[] vetor) {
+        DecimalFormat df = new DecimalFormat("0.000");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("O valor atualizado " + (i + 1) + " é: " + df.format(vetor[i]));
+        }
     }
-    
 
     public static void main(String[] args) {
-       new Uni05Exe03(); 
+        new Uni05Exe03();
     }
 }
