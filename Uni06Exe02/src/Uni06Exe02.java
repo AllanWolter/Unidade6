2b512bb2db8a8e21e4e11fdd62e2f959dd2aa862
AllanWolter <desenvolvimentoallanbs@gmail.com>
2026-05-11 10:17:41 -0300

    exe02

---
+++ Uni06Exe02/src/Uni06Exe02.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Uni06Exe02 {
+    public Uni06Exe02(){
+        Scanner sc = new Scanner(System.in);
+       double vetor[] = new double[12];
+
+        lervalores(sc, vetor);
+
+        double media = calcularmedia(vetor);
+
+        valormaiormedia(vetor, media);
+
+        sc.close();
+    }
+    private void lervalores(Scanner sc, double vetor[]) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Digite o " + (i + 1) +"º valor: ");
+            vetor[i] = sc.nextDouble();
+        }    
+    }
+    private double calcularmedia(double vetor[]){
+        double soma = 0;
+         for (int i = 0; i < vetor.length; i++) {
+            soma += vetor[i];
+        }
+        double media = soma / (vetor.length);
+        for (int i = vetor.length - 1; i >= 0; i--) {
+            System.out.println("Valor da posição " + (i + 1) + ": " + vetor[i]);
+        }
+        return media;
+    }
+    private void valormaiormedia(double vetor[], double media){
+        for (int i = vetor.length - 1; i >= 0; i--){
+            if (vetor[i] > media) {
+                System.err.println("O valor " + vetor[i] +" é maior que a média");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Uni06Exe02();
+    }
+}
