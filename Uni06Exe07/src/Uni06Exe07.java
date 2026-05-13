b5699187f789d22a6e61d51892105050dcdb8c62
AllanWolter <desenvolvimentoallanbs@gmail.com>
2026-05-13 10:47:00 -0300

    exe07

--- Uni06Exe07/src/Uni06Exe07.java
+++ Uni06Exe07/src/Uni06Exe07.java
@@ -2,20 +2,21 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Uni06Exe07 {
-    public Uni06Exe07(){
+    public Uni06Exe07() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite a quantidades de valores para o vetor: ");
         int n = sc.nextInt();
         while (n > 20 || n < 1) {
             System.out.println("Digite um valor até 20 posições: ");
-            n = sc.nextInt(); 
+            n = sc.nextInt();
         }
         int vetor[] = new int[n];
         lerValores(vetor, sc);
         escreverValores(vetor);
     }
+
     private boolean duplicado(int vetor[], int ate, int valor) {
-        for (int j = 0; j < ate; j++) { 
+        for (int j = 0; j < ate; j++) {
             if (vetor[j] == valor) {
                 return true;
             }
@@ -33,26 +34,18 @@ public class Uni06Exe07 {
             }
         }
     }
+
     private void escreverValores(int vetor[]) {
         System.out.print("Vetor origem: ");
         for (int i = 0; i < vetor.length; i++) {
-        System.out.print(  "[" + vetor[i] + "]");
+            System.out.print("[" + vetor[i] + "]");
         }
         System.out.print("\nVetor ordenado: ");
         Arrays.sort(vetor);
         for (int i = 0; i < vetor.length; i++) {
-        System.out.print(  "[" + vetor[i] + "]");
+            System.out.print("[" + vetor[i] + "]");
         }
     }
-            
-
-            
-    
-        
-            
-        
-            
-        
 
     public static void main(String[] args) throws Exception {
         new Uni06Exe07();
